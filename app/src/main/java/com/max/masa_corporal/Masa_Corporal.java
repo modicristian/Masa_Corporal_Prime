bf3979618d4c1f1d56cde58547b398d0cbf84b3c
max <vloxomire@gmail.com>
2018-10-04 20:41:39 -0300

    inicio

---
+++ app/src/main/java/com/max/masa_corporal/Masa_Corporal.java
@@ -0,0 +1,13 @@
+package com.max.masa_corporal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Masa_Corporal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acty_masa__corporal);
+    }
+}
