6e1e3f8af0ae91a99413e5e46153082b0d65b632
max <vloxomire@gmail.com>
2018-10-04 21:09:39 -0300

    2109

--- app/src/main/java/com/max/masa_corporal/Masa_Corporal.java
+++ app/src/main/java/com/max/masa_corporal/Masa_Corporal.java
@@ -2,12 +2,22 @@ package com.max.masa_corporal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Masa_Corporal extends AppCompatActivity {
+    private EditText edtPeso,edtAltura,edtEdad;
+    private Spinner spinGenero;
+    private Button btoCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acty_masa__corporal);
+
+        edtPeso=findViewById(R.id.edt_Peso_Id);
+        edtAltura=findViewById(R.id.edt_Altura)
     }
 }
