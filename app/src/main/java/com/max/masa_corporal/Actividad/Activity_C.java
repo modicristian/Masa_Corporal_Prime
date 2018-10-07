d2f6092a7d141b3efd3076f62c9a89815645cf2f
Max <vloxomire@gmail.com>
2018-10-07 20:47:30 -0300

    2047
    Agrego 2 activitys

---
+++ app/src/main/java/com/max/masa_corporal/Actividad/Activity_C.java
@@ -0,0 +1,15 @@
+package com.max.masa_corporal.Actividad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.max.masa_corporal.R;
+
+public class Activity_C extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acty__c);
+    }
+}
