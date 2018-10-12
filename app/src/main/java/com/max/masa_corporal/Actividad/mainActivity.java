e6ea61cbca05097d895213f6786a9ebb5295363c
max <vloxomire@gmail.com>
2018-10-11 22:04:29 -0300

    2203

--- app/src/main/java/com/max/masa_corporal/Actividad/mainActivity.java
+++ app/src/main/java/com/max/masa_corporal/Actividad/mainActivity.java
@@ -14,11 +14,19 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.max.masa_corporal.R;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class mainActivity extends AppCompatActivity {
@@ -67,6 +75,7 @@ public class mainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Masa Corporal");    //titulo en la barra de accion
         getSupportActionBar().setLogo(R.drawable.ic_launcher_foreground);   //logo
     }
+
     public void onClick(View view){
 
         //Shared
@@ -85,6 +94,8 @@ public class mainActivity extends AppCompatActivity {
         editor.putLong("genero",genero);
         editor.putString("edad",edad);
         editor.commit();
+
+        new jsonMasa(peso, altura, genero, edad).execute("https://bmi.p.mashape.com/");
 
         txtResulPeso.setText(peso+" Kg ");
         txtResulAltura.setText(altura+" Cm ");
@@ -114,33 +125,84 @@ public class mainActivity extends AppCompatActivity {
         }
         return true;
     }
+
     public class jsonMasa extends AsyncTask<String,Void,String>{
 
-        @Override
-        protected String doInBackground(String... url) {
-            InputStream inputStream = null;
-            String resultado=" ";
-            try{
-                inputStream=new URL(url[0]).openStream();
-                if(inputStream != null) {
-                    BufferedReader buffer = new BufferedReader( new InputStreamReader(inputStream));
-                    String line = "";
-                    while ((line = buffer.readLine()) != null)
-                        resultado += line;
+        String data = " ";
 
-                    inputStream.close();
-                } else {
-                    // ERROR;
+        jsonMasa(String peso, String altura, Long genero,String edad){
+            String generoJson="";
+            if(genero==1){generoJson="m"}
+            if(genero==2){generoJson="f"}
+            data = "{ \"weight\": { \"value\": \""+peso+"\", \"unit\": \"kg\" }, \"height\": { \"value\": \""+altura+"\", \"unit\": \"cm\" }, \"sex\": \""+generoJson+"\", \"age\": \""+edad+"\",\"waist\": \"34.00\", \"hip\": \"40.00\" }\n";
+
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            InputStream inputStream = null;
+            String result = "";
+            HttpURLConnection httpcon;
+
+
+            try {
+                //Connect
+                httpcon = (HttpURLConnection) ((new URL (urls[0]).openConnection()));
+                httpcon.setDoOutput(true);
+                httpcon.setRequestProperty("X-Mashape-Key", "hRtSku8fDqmsh7SAIsKMaGABTE9up1iXrzjjsnJUbVJImKtDfF");
+                httpcon.setRequestProperty("Content-Type", "application/json");
+                httpcon.setRequestProperty("Accept", "application/json");
+                httpcon.setRequestMethod("POST");
+                httpcon.connect();
+
+                //Write
+                OutputStream os = httpcon.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(data);
+                writer.close();
+                os.close();
+
+                //Read
+                BufferedReader br = new BufferedReader(new InputStreamReader(httpcon.getInputStream(),"UTF-8"));
+
+                String line = null;
+                StringBuilder sb = new StringBuilder();
+
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
                 }
+
+                br.close();
+                result = sb.toString();
 
             } catch (Exception e) {
                 // ERROR;
                 Log.d("InputStream", e.getLocalizedMessage());
             }
-            return resultado;
+            return result;
         }
 
-    }
+        @Override
+        protected void onPostExecute(String s) {
+
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+                JSONObject jsonBmi=jsonObject.getJSONObject("bmi");
+                String valuej =jsonBmi.getString("value");
+                String statusj= jsonBmi.getString("status");
+                String riskj= jsonBmi.getString("risk");
+
+
+
+
+
+            } catch (Exception e) {
+
+                // Cualquier problema en la lectura del JSON, se ir� por este camino.
+                Toast.makeText(mainActivity.this, "Hubo un problema", Toast.LENGTH_LONG).show();
+            }
+
         }
     }
-}
+    }
+
