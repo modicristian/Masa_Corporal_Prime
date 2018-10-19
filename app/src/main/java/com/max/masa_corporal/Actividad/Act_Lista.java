60b86e57f35cf30de03c23ed0d12670502e84ac5
max <vloxomire@gmail.com>
2018-10-18 21:56:45 -0300

    2156

--- app/src/main/java/com/max/masa_corporal/Actividad/Act_Lista.java
+++ app/src/main/java/com/max/masa_corporal/Actividad/Act_Lista.java
@@ -2,14 +2,19 @@ package com.max.masa_corporal.Actividad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.max.masa_corporal.R;
 
 public class Act_Lista extends AppCompatActivity {
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acty_c);
+
+        listView = findViewById(R.di.listView);
     }
 }
---
+++ app/src/main/java/com/max/masa_corporal/Adapter/AdapterListView.java
@@ -0,0 +1,34 @@
+package com.max.masa_corporal.Adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class AdapterListView extends BaseAdapter {
+    private Context context;
+
+    public AdapterListView(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
